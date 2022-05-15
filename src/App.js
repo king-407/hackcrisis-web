a9aacb537dadf4f1d79537ab325463dd419e8428
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-15 15:51:35 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./Components/Navbar";
 import "./App.css";
-
+import Body from "./Components/Body";
 function App() {
   return (
     <div>
       <Navbar />
       <div className="container">
-        
       </div>
+      {/* <Body /> */}
     </div>
   );
 }
---
+++ src/Components/Body.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Body.css'
+export default function Hi() {
+  return (
+    <div className="main">Hi</div>
+  )
+}
